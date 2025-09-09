5381dbea35bd3f9e6ad32710149a31626c56ed16
kOOnzTe <alperwithsteel@gmail.com>
2025-09-09 21:49:14 +0300

    jest config updated to remove unrelated fails

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: [],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@reduxjs/toolkit|react-redux|@react-navigation|react-native|@react-native|immer)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
 };
