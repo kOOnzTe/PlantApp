401c915e6381f5ca611908035494b449898941f6
kOOnzTe <alperwithsteel@gmail.com>
2025-09-09 13:57:58 +0300

    redux store setup with persistence, asyn storage and dummy API integration with their types made

---
+++ src/store/api/plantsApi.ts
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type {
+  CategoriesResponse,
+  QuestionsResponse,
+} from '../../types/api.types';
+
+export const plantsApi = createApi({
+  reducerPath: 'plantsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://dummy-api-jtg6bessta-ey.a.run.app/',
+    prepareHeaders: headers => {
+      headers.set('Content-Type', 'application/json');
+      return headers;
+    },
+  }),
+  tagTypes: ['Categories', 'Questions'],
+  endpoints: builder => ({
+    getCategories: builder.query<CategoriesResponse, void>({
+      query: () => 'getCategories',
+      providesTags: ['Categories'],
+      transformResponse: (response: CategoriesResponse) => {
+        return {
+          ...response,
+          data: response.data.sort((a, b) => a.rank - b.rank),
+        };
+      },
+    }),
+    getQuestions: builder.query<QuestionsResponse, void>({
+      query: () => 'getQuestions',
+      providesTags: ['Questions'],
+      transformResponse: (response: QuestionsResponse) => {
+        return response.sort((a, b) => a.order - b.order);
+      },
+    }),
+  }),
+});
+
+export const { useGetCategoriesQuery, useGetQuestionsQuery } = plantsApi;
